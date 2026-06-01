5039c5c22dd1d5b3978b568e377da0fa052ee89f
Vishal Dolasia <vishaldolasiawork@gmail.com>
2026-06-01 18:06:38 +0530

    Update navbar and routing

---
+++ src/components/Button.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function button({
+    children,
+    type='button',
+    bgColor = 'bg-blue-600',
+    textColor ='text-white',
+    classname = '',
+    ...props
+
+}) {
+  return (
+    <button className={`px-4 py-2 rounded-lg ${classname} ${bgColor} ${classname}`} {...props}>
+        {children}
+    </button>
+  )
+}
+
+export default button
